191ce2f9b68216966df64547af1008c0ae228ef8
Alan Trebugeais <alan.trebugeais@epitech.eu>
2026-01-07 01:58:26 +0100

    Update: added a debug log and a warning if the bmFont.Info.Size is under or equal to 0 (at the end of TryInitializeFromFile in BmFontDescription)

--- Operators/Lib/Utils/BmFontDescription.cs
+++ Operators/Lib/Utils/BmFontDescription.cs
@@ -35,7 +35,12 @@ internal sealed class BmFontDescription
                 return false;
             }
             
-            Log.Debug("loaded font with character count:" + bmFont.Chars.Length);
+            // Warn if size is 0 - this will cause rendering issues
+            if (bmFont.Info?.Size <= 0)
+            {
+                Log.Warning($"Font size is {bmFont.Info.Size} in font file '{filepath}' - this will cause text rendering issues! Please check the font file's <info size=...> attribute.");
+            }
+            Log.Debug($"Loaded font '{filepath}', {bmFont.Chars?.Length} characters, {bmFont.Kernings?.Length} kernings");
             stream.Close();
         }
         catch (Exception e)
